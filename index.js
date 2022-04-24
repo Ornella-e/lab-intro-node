7c621ba05e10f93568b05db8c0dcde9a5911c833
Ornella Escalada Corsi <ornellaescaladacorsi@Ornellas-Air.fritz.box>
2022-04-24 11:20:14 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b)=>{
+      return a-b;
+    })
+    this.length ++;
+  }
 
-  get(pos) {}
+  get(pos) {
+  if(!this.length){
+    throw new Error('OutOfBounds');
+  }
+    return this.items.indexOf(pos);
+  }
 
-  max() {}
+  max() {
+    if(!this.length){
+    throw new Error('EmptySortedList');
+  }
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {    
+    if(!this.length){
+    throw new Error('EmptySortedList');
+  }
+    return Math.min(...this.items);
+  } 
+  
+  sum() {
+    if(!this.length){
+      return 0;
+    }
+    const sum = this.items.reduce((a,b)=>{
+     return a + b;
+    });
+    return sum; 
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if(!this.length){
+      throw new Error('EmptySortedList');
+    }
+    const sum = this.items.reduce((a,b)=>{
+      return a + b;
+     });
+const avg = sum/this.length;
+return avg;
+  }
 }
 
 module.exports = SortedList;
